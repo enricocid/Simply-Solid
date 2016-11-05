28dd2cc49b547cee017d36fd53d2b53fc8370c06
EnricoD <enrico2588@gmail.com>
2016-11-05 18:32:42 +0100

    update material-dialogs

--- project/app/src/main/java/com/enrico/earthquake/AboutDialog.java
+++ project/app/src/main/java/com/enrico/earthquake/AboutDialog.java
@@ -1,20 +1,15 @@
 package com.enrico.earthquake;
 
 import android.app.Dialog;
-import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
-import android.text.Html;
-import android.text.Spanned;
 
 import com.afollestad.materialdialogs.MaterialDialog;
 
 
 public class AboutDialog extends DialogFragment {
-
-    Spanned result;
 
     public static void show(AppCompatActivity context) {
         AboutDialog dialog = new AboutDialog();
@@ -25,16 +20,10 @@ public class AboutDialog extends DialogFragment {
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
 
-        if (Build.VERSION.SDK_INT >= 24) {
-            result = Html.fromHtml(getString(R.string.about_body), Html.FROM_HTML_MODE_LEGACY);
-        } else {
-            result = Html.fromHtml(getString(R.string.about_body));
-        }
-
         return new MaterialDialog.Builder(getActivity())
                 .title(R.string.about)
                 .positiveText(R.string.dismiss)
-                .content(result)
+                .content(R.string.about_body)
                 .contentLineSpacing(1.6f)
                 .build();
     }
